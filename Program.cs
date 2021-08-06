1bc2d0af2e05f61a6983b580265cee14473d31af
Marco <antnyhern@gmail.com>
2021-08-05 23:26:12 -0500

    All proyect

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskExampole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var task = new Task(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Task");
+            });
+            task.Start();
+
+            await task;
+
+            Console.WriteLine("Hola mundo");
+
+            Console.ReadKey();
+        }
+    }
+}
